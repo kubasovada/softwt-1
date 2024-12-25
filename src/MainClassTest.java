feb52183b45f87bfa427988edcdc7a1c5c9a10e8
Darya <kubasovada@mail.ru>
2024-12-25 19:30:49 +0300

    lesson1-test2

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -2,10 +2,18 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MainClassTest {
+    MainClass localNumber = new MainClass();
     @Test
     public void testGetLocalNumber() {
-        MainClass localNumber = new MainClass();
         int number = localNumber.getLocalNumber();
         Assert.assertEquals("Value is not 14", 14, number);
     }
-}
+
+    @Test
+    public void testGetClassNumber() {
+        int classNumber = localNumber.getClassNumber();
+        Assert.assertTrue("classNumber less 45 ("+classNumber + "), but must be > 45" , classNumber > 45);
+
+    }
+
+    }
